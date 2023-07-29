d1ff79ed011f51275494f0ebf1941fc8374b1a3f
threeSeven <nuomibaicha@163.com>
2023-07-29 21:14:52 +0800

    update 修改启动图标

--- src/main/java/com/yidiansishiyi/searchCenter/controller/WxMpController.java
+++ src/main/java/com/yidiansishiyi/searchCenter/controller/WxMpController.java
@@ -86,47 +86,4 @@ public class WxMpController {
         }
     }
 
-    /**
-     * 设置公众号菜单
-     *
-     * @return
-     * @throws WxErrorException
-     */
-    @GetMapping("/setMenu")
-    public String setMenu() throws WxErrorException {
-        log.info("setMenu");
-        WxMenu wxMenu = new WxMenu();
-        // 菜单一
-        WxMenuButton wxMenuButton1 = new WxMenuButton();
-        wxMenuButton1.setType(MenuButtonType.VIEW);
-        wxMenuButton1.setName("主菜单一");
-        // 子菜单
-        WxMenuButton wxMenuButton1SubButton1 = new WxMenuButton();
-        wxMenuButton1SubButton1.setType(MenuButtonType.VIEW);
-        wxMenuButton1SubButton1.setName("跳转页面");
-        wxMenuButton1SubButton1.setUrl(
-                "https://yupi.icu");
-        wxMenuButton1.setSubButtons(Collections.singletonList(wxMenuButton1SubButton1));
-
-        // 菜单二
-        WxMenuButton wxMenuButton2 = new WxMenuButton();
-        wxMenuButton2.setType(MenuButtonType.CLICK);
-        wxMenuButton2.setName("点击事件");
-        wxMenuButton2.setKey(WxMpConstant.CLICK_MENU_KEY);
-
-        // 菜单三
-        WxMenuButton wxMenuButton3 = new WxMenuButton();
-        wxMenuButton3.setType(MenuButtonType.VIEW);
-        wxMenuButton3.setName("主菜单三");
-        WxMenuButton wxMenuButton3SubButton1 = new WxMenuButton();
-        wxMenuButton3SubButton1.setType(MenuButtonType.VIEW);
-        wxMenuButton3SubButton1.setName("编程学习");
-        wxMenuButton3SubButton1.setUrl("https://yupi.icu");
-        wxMenuButton3.setSubButtons(Collections.singletonList(wxMenuButton3SubButton1));
-
-        // 设置主菜单
-        wxMenu.setButtons(Arrays.asList(wxMenuButton1, wxMenuButton2, wxMenuButton3));
-        wxMpService.getMenuService().menuCreate(wxMenu);
-        return "ok";
-    }
 }
